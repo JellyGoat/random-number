e4e003e26016a146d507aa156141dc9827047338
Jelly-Goat <3208861439@qq.com>
2021-01-23 04:38:27 +0800

    not a big different in UI design, but rewrite anything from window to button

--- jrs.py
+++ jrs.py
@@ -5,98 +5,241 @@ import os
 from sys import argv
 from random import shuffle
 from tkinter.messagebox import *
+import copy
+import pygal
 
 
 # create the screen
-window = tk.Tk("Random Stand Up!")
+window = tk.Tk()
 window.geometry("800x600")
-
-# title lable
-Title_Lable = tk.Label(window, text="抽号器 version 0.1.0", bg="pink",
-                       font=("Fira code", 12), width=30, height=2)
-Title_Lable.pack()
-# Name echo lable
-Random_Name = tk.StringVar()
-Random_Name.set("下一位：等待您的抽号")
-Name_Lable = tk.Label(window, textvariable=Random_Name, bg="pink",
-                      font=("等线 Light", 14), width=30, height=3)
-Name_Lable.place(x=250, y=192, anchor="nw")
-
-'''
-                       <-- 分割： 按钮 ->
-'''
-
-# let you to input the names
+window.title("Random Selector | 随机抽取器 version 1.0.0")
+window.iconphoto(False, tk.PhotoImage(
+    file=os.path.join(".", "datas", "icon.png")))
 
 
-def Disabled():
-    pass
+# Global varies
+global select_pool
+select_pool = list([])
+global selecting_pool
+selecting_pool = list([])
+global frequency
+frequency = dict({})
+global selecting_frequency
+selecting_frequency = dict({})
+global total_count
+total_count = 0
+global selecting_count
+selecting_count = 0
+global freq_diagram
+freq_diagram = pygal.Bar()
+freq_diagram._title = "抽取池的频数分布直方图"
+freq_diagram._x_title = "抽取池元素"
+freq_diagram._y_title = "元素剩余（或已抽取）频数"
 
 
-Name_List = [None]
-Name_List.pop()
+# button functions
 
 
-def Search_File(path: str, file_name: str):
-    files = os.listdir(path)
-    if file_name in files:
-        return True
-    else:
-        return False
+def Exit_Button_Onclick():
+    # exit the program
+    exit(0)
 
 
-def Create_File(path: str, file_name: str):
-    fcreate = open(os.path.join(path, file_name), "w+")
-    fcreate.close()
+def Refresh_Button_Onclick():
+    # refresh this turn through this function
+    global selecting_pool
+    global selecting_frequency
+    global select_pool
+    global frequency
+    selecting_pool = copy.deepcopy(select_pool)
+    shuffle(selecting_pool)
+    selecting_frequency = copy.deepcopy(frequency)
 
 
-def Open_Names_File():
-    global Name_List
-    file_name = os.path.join(".", "names.txt")
-    if not Search_File(".", "names.txt"):
-        Create_File(".", "names.txt")
-    os.system(file_name)
-    fin = open(file_name, "r")
-    Name_List = fin.read().split()
-    shuffle(Name_List)
-    if "--debug" in argv:
-        print(Name_List)
+def Reread_Button_Onclick():
+    # reread anything from the choices.txt
+    # but anything left this turn is preserved
+    global select_pool
+    global frequency
+    global freq_diagram
+    fin = open(os.path.join(".", "datas", "choices.txt"), mode="r+")
+    select_pool = fin.readlines()
+    fin.close()
+    for alternative in select_pool:
+        alternative = alternative.strip()
+        if alternative is "":
+            while alternative in select_pool:
+                select_pool.remove(alternative)
+    select_pool.sort()
+    frequency.clear()
+    for alternative in set(select_pool):
+        frequency[alternative] = select_pool.count(alternative)
+    freq_diagram.x_labels = list(set(select_pool))
 
 
-Input_Name_Button = tk.Button(window, text="更新或输入名字", bg="lightblue",
-                              font=("等线 Light", 12), width=18, height=2,
-                              command=Open_Names_File, activebackground="pink",
-                              relief="flat")
-Input_Name_Button.place(x=50, y=100, anchor="nw")
-# next name button
+def Save_Button_Onclick():
+    global select_pool
+    fout = open(os.path.join(".", "datas", "choices.txt"), mode="w+")
+    for alternative in select_pool:
+        fout.write(str(alternative) + '\n')
+    fout.close()
 
 
-def Echo_Next():
-    if len(Name_List) is not 0:
-        Random_Name.set(Name_List[0])
-        del Name_List[0]
-    else:
-        Random_Name.set("本轮已结束，请更新列表")
+def Rend_freq_to_file():
+    global freq_diagram
+    freq_list = list([])
+    for selection, count in frequency.items():
+        freq_list.append(count)
+    freq_diagram.add("总抽取池", copy.deepcopy(freq_list))
+    freq_list.clear()
+    for left_freq in selecting_frequency.values():
+        freq_list.append(left_freq)
+    freq_diagram.add("剩余抽取池", copy.deepcopy(freq_list))
+    freq_list.clear()
+    for selection in frequency.keys():
+        freq_list.append(frequency[selection] - selecting_frequency[selection])
+    freq_diagram.add("已抽取数量", copy.deepcopy(freq_list))
+    del freq_list
+    freq_diagram.render_in_browser()
+    freq_diagram = pygal.Bar()
+    freq_diagram._title = "抽取池的频数分布直方图"
+    freq_diagram._x_title = "抽取池元素"
+    freq_diagram._y_title = "元素剩余（或已抽取）频数"
 
 
-Next_Button = tk.Button(window, text="抽下一位", bg="lightblue",
-                        font=("等线 Light", 12), width=18, height=2,
-                        command=Echo_Next, activebackground="pink",
-                        relief="flat")
-Next_Button.place(x=50, y=200, anchor="nw")
-# remove temp button
+# buttons
+# exit button
+exit_button = tk.Button(
+    window,
+    text="退出程序",
+    bg="lightblue",
+    fg="black",
+    font=("微软雅黑", 14),
+    width=15,
+    height=1,
+    relief="flat",
+    command=Exit_Button_Onclick
+)
+exit_button.place(
+    x=50,
+    y=500,
+    anchor="nw"
+)
+# refresh button
+refresh_button = tk.Button(
+    window,
+    text="刷新本次抽取池",
+    bg="lightblue",
+    fg="black",
+    font=("微软雅黑", 14),
+    width=15,
+    height=1,
+    relief="flat",
+    command=Refresh_Button_Onclick
+)
+refresh_button.place(
+    x=50,
+    y=150,
+    anchor="nw"
+)
+# Run the function first
+# Reread_Button_Onclick()
+# reread button
+reread_button = tk.Button(
+    window,
+    text="重新读取抽取池",
+    bg="lightblue",
+    fg="black",
+    font=("微软雅黑", 14),
+    width=15,
+    height=1,
+    relief="flat",
+    command=Reread_Button_Onclick
+)
+reread_button.place(
+    x=50,
+    y=210,
+    anchor="nw"
+)
+
+# Labels
+# Main title
+main_title = tk.Label(
+    window,
+    text="Random Selector | 随机抽取器",
+    font=("等线", 20),
+    width=40,
+    height=2,
+    bg="pink",
+    anchor="center"
+)
+main_title.place(
+    x=400,
+    y=10,
+    anchor="n"
+)
+# selected subtitle
+subtitle = tk.Label(
+    window,
+    text="随机抽取池信息",
+    font=("等线", 17),
+    width=20,
+    height=1,
+    bg="pink"
+)
+subtitle.place(
+    x=500,
+    y=100,
+    anchor="n"
+)
+# former selection
+former_side_lable = tk.Label(
+    window,
+    text="上一次选择",
+    font=("微软雅黑", 14),
+    width=13,
+    height=2,
+    bg="pink",
+    anchor="center"
+)
+former_side_lable.place(
+    x=380,
+    y=150,
+    anchor="n"
+)
+# former selection content
+former_content = tk.Label(
+    window,
+    text="这里还什么都没有选٩(๑>◡<๑)۶",
+    width=23,
+    height=2,
+    font=("微软雅黑", 14),
+    bg="cornsilk",
+    anchor="center"
+)
+former_content.place(
+    x=620,
+    y=150,
+    anchor="n"
+)
 
 
-def Remove_Temp_File():
-    try:
-        os.remove(os.path.join(".", "names.txt"))
-    except FileNotFoundError:
-        showerror("错误", "还没有生成临时文件")
+# add menu to this selector
+menu_bar = tk.Menu(window)
+# file operations
+file_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="文件", menu=file_menu)
+file_menu.add_command(
+    label="保存当前抽取池到配置文件",
+    command=Save_Button_Onclick
+)
+file_menu.add_command(
+    label="从配置中读取抽取池",
+    command=Reread_Button_Onclick
+)
 
-Remove_Button = tk.Button(window, text="删除临时文件", bg="lightblue",
-                          font=("等线 Light", 12), width=18, height=2,
-                          command=Remove_Temp_File, activebackground="pink",
-                          relief="flat")
-Remove_Button.place(x=50, y=300, anchor="nw")
 
+# something else to complete
+window.config(menu=menu_bar)
+# main loop here
 window.mainloop()
